1f2a4fe93344978b09a5fc0fcf82c6e7a6a014d0
Stalyn0101 <yixon11@gmail.com>
2024-12-01 23:29:44 -0500

    repositorio

--- src/main/java/ec/edu/espoch/trabajogrupal/Auto/Auto.java
+++ src/main/java/ec/edu/espoch/trabajogrupal/Auto/Auto.java
@@ -4,10 +4,21 @@
  */
 package ec.edu.espoch.trabajogrupal.Auto;
 
+import java.awt.Color;
+
 /**
  *
  * @author USER
  */
 public class Auto {
-    
+    private String Marca;
+    private int Modelo;
+    private double Motor;
+    private TipoCombus tipoCombus;
+    private TipoAuto tipoAuto;
+    private int numPuertas;
+    private int numAsientos;
+    private double velocMaxima;
+    private Color color;
+    private double velocActual = 0;
 }
